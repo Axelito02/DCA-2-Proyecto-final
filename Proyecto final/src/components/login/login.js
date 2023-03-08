a68293482f5fc7567e5e28c3290d9d6198c64794
Kazura <isaacmont88@gmail.com>
2023-03-08 02:05:24 -0500

    Agregada imágen de fondo, intento de botones y login

--- Proyecto final/src/components/login/login.js
+++ Proyecto final/src/components/login/login.js
@@ -11,22 +11,32 @@ class login extends HTMLElement {
 
     render(){
         this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="./styles/style-login.css">
         <div class="Login-both">
-        <section id ="images">
-            <img src="https://i.pinimg.com/originals/bd/64/79/bd6479fe38844545f7a06223bf927015.gif" alt="Mario world gif">
+        <section id ="space">
         </section>
         <section id ="login">
             <div class="card">
-                <input type="text">
-                <input type="text">
+                <input type="text class="username" placeholder="Enter username">
+                <input type="text" class="password" placeholder="Enter password">
                 <button></button>
                 <p>or sign in with</p>
-                <button></button>
-                <button></button>
+                <div class="facebook-btn">
+  <div class="facebook-icon-wrapper">
+    <img class="facebook-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Logo_de_Facebook.png/899px-Logo_de_Facebook.png"/>
+  </div>
+  <p class="btn-text"><b>Sign in with facebook</b></p>
+
+                <div class="google-btn">
+  <div class="google-icon-wrapper">
+    <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+  </div>
+  <p class="btn-text"><b>Sign in with google</b></p>
+                <p>Don't have an account?</p>
+                
             </div>
         </section>
     </div>
-    <link rel="stylesheet" src="../src/components/login/styleLogin.css"> ??
         `;
     }
 
